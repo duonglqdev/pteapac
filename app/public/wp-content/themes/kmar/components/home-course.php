41624e03042a83861d1cb23793c59ce9869d7013
duongdev <duonglq.dev@gmail.com>
2024-04-19 18:05:39 +0700

    update theme

--- app/public/wp-content/themes/kmar/components/home-course.php
+++ app/public/wp-content/themes/kmar/components/home-course.php
@@ -1,51 +1,61 @@
 <section class="scroll--section slide swiper-slide  course--home course--home1">
-	<div class="box-wp">
+	<div class="container">
 		<div class="cus-course--home clearfix">
 			<div class="top-course--home top--tl--home"> 
             <?php
 					$title = get_sub_field( 'clone_title' );
 					?>
 					<?php if ( $title ) : ?>
-						<label class="label-mm t1l mx-auto ">
+						<label class="label-mm t1l ">
 							<?php echo $title['clone_title_top']; ?>
 						</label>
                         <h2 class="title-mm"><?php echo $title['clone_title_main']; ?></h2>
 					<?php endif; ?>
 
 			</div>
-            <?php if( have_rows('...') ): ?>
-                <div class="slides">
-                <?php while( have_rows('...') ): the_row();?>
-                    <li>
-                        <p><?php the_sub_field('caption'); ?></p>
-                    </li>
+            <?php if( have_rows('f4_list_course') ): ?>
+                <div class="list-course--home  clearfix">
+                <?php while( have_rows('f4_list_course') ): the_row();
+                $btn_contact = get_sub_field('btn_contact');
+           
+                
+                ?>
+                  
+                    <div class="item-course--home">
+                        <div class="cus-i-course--home">
+                            <div class="img-course">
+                         <?php echo wp_get_attachment_image(get_sub_field('img'), 'full') ?>      
+                        </div>
+                            <div class="intro--course">
+                                <div class="top-course--home clearfix">
+
+                                    <div class="left-course--home text-center"><span class="mx-auto"><?php the_sub_field('level_course'); ?></span> </div>
+
+                                    <div class="right-course--home">
+                                        <?php
+                                            if ($btn_contact['clone_btn']['title'] ) : ?>
+                                           
+                                            <a href="<?php echo$btn_contact['clone_btn']['link']; ?>"
+                                            title="<?php echo$btn_contact['clone_btn']['title']; ?>"
+                                            class="a-more-course--home"><span><?php echo$btn_contact['clone_btn']['title']; ?></span><i class="fa-regular fa-arrow-right"></i>
+                                            </a>
+                                        <?php endif; ?>
+                                       
+                                    </div>
+                                </div>
+                                <h3 class="name-course--home">
+                                    <a href="tieng-anh-mam-non-3-6-tuoi"
+                                        class="t2l" title="<?php the_sub_field('title_course');  ?>">
+                                      <?php the_sub_field('title_course');  ?>
+                                    </a>
+                                    </h3>
+                            </div>
+                        </div>
+                    </div>
                 <?php endwhile; ?>
                 </div>
             <?php endif; ?>
-			<div class="list-course--home  clearfix">
-				<div class="item-course--home">
-					<div class="cus-i-course--home">
-						<div class="img-course">
-                            <img src="images/course_404.png"
-								alt="Tiếng Anh Mầm Non (3-6 tuổi)"></div>
-						<div class="intro--course">
-							<div class="top-course--home clearfix">
-								<div class="left-course--home"><span>SMART
-										KIDS</span> </div>
-								<div class="right-course--home"><a href="tieng-anh-mam-non-3-6-tuoi"
-										title="Tiếng Anh Mầm Non (3-6 tuổi)"
-										class="a-more-course--home"><span>Khám
-											phá</span><i class="fa-regular fa-arrow-right"></i></a>
-								</div>
-							</div>
-							<h3 class="name-course--home"><a href="tieng-anh-mam-non-3-6-tuoi"
-									class="t2l" title="Tiếng Anh Mầm Non (3-6 tuổi)">Tiếng Anh
-									Mầm Non (3-6 tuổi)</a></h3>
-						</div>
-					</div>
-				</div>
-				
-			</div>
+		
 		</div>
 	</div>
 </section>
